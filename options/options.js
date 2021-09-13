087f43f3365ef6d2fc70cc5693c201708d9fd187
Crowdbotics <team@crowdbotics.com>
2021-09-13 16:46:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "late_band_30513Identifier",
+  url: "https://late_band_30513Identifier.botics.co",
+  api: "https://late_band_30513Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
